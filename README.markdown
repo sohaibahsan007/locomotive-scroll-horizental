d97b6137724482b0d3fb4534ea62f8fd6ae1ca7b
Sohaib Ahsan <sohaibahsan007@live.com>
2022-01-06 18:20:10 +0500

    Release.

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# Locomotive Scroll  - horizontal demo
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Pullapooh/pen/dyOVmNK](https://codepen.io/Pullapooh/pen/dyOVmNK).
+
+
---
+++ dist/script.js
@@ -0,0 +1,30 @@
+let locoScrollDefaults = {
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true,
+    direction: "horizontal",
+};
+
+let locoScroll = new LocomotiveScroll(locoScrollDefaults);
+
+
+
+
+// GSAP integration
+gsap.registerPlugin(ScrollTrigger);
+
+locoScroll.on("scroll", ScrollTrigger.update);
+
+ScrollTrigger.scrollerProxy(".smoothScroll", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  },
+  scrollLeft(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.x;
+  },
+
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+
+  pinType: document.querySelector(".smoothScroll").style.transform ? "transform" : "fixed"
+});
